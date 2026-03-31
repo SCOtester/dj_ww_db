887a42e80a1764bfc0105bd0a6626379e7721eb0
Алексей Безденежных <60113213@lemanapro.ru>
2026-03-31 21:55:31 +0300

    model_created

--- phones/models.py
+++ phones/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    image = models.URLField(max_length=500)
+    price = models.PositiveIntegerField() # цена не может быть отрицательной и копейки вряд ли нужны
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(max_length=255, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+    #pass - по идеи "заглушка" больше не нужна
